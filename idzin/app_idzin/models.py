6deb5ec6e0542867f2becb5aebedb01b6360c802
Georgij <7364400@gmail.com>
2022-10-06 13:56:12 +0300

    Data for hexagrams appended.

--- idzin/app_idzin/models.py
+++ idzin/app_idzin/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Idzin(models.Model):
+    code_idzin = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=4000)
+
